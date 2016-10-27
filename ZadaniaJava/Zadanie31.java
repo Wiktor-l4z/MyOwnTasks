3e53ffa9b11b57e5ca010d683ba3ad88349eb946
Wiktor-l4z <wiktorkalinowski@gmail.com>
2016-10-27 21:11:34 +0200

    Zadania Mirosław J.Kubiak
    Zadania Mirosław J.Kubiak

---
+++ ZadaniaJava/Zadanie31.java
@@ -0,0 +1,13 @@
+
+public class Zadanie31 {
+
+    public static void main(String[] args) {
+        int x,y;
+
+        for (x=0; x<10; x++);
+        {
+            y=3*x;
+            System.out.println("x = " + x + '\t' + "y = " + y);
+        }
+    }
+}
---
+++ ZadaniaJava/Zadanie33.java
@@ -0,0 +1,15 @@
+
+public class Zadanie33 {
+
+    public static void main(String[] args) {
+        int x=0,y=0;
+
+         while ( x<=10)
+        {
+
+            y = 3 * x;
+            System.out.println("x = " + x + '\t' + "y = " + y);
+        x++;
+        }
+    }
+}
