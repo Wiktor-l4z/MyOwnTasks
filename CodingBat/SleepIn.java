e2d7fc8ed527c67af3180333f6a5148c4c17fa77
Wiktor <wiktorkalinowski@gmail.com>
2016-12-22 20:29:22 +0100

    add more tasks

--- CodingBat/SleepIn.java
+++ CodingBat/SleepIn.java
@@ -1,5 +1,5 @@
 
-public class Warmup-1 {
+public class Warmup-1{
 
 //    The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
 //
@@ -7,25 +7,25 @@ public class Warmup-1 {
 //    sleepIn(true, false) → false
 //    sleepIn(false, true) → true
 
-    public boolean sleepIn(boolean weekday, boolean vacation) {
-        if (!weekday || vacation){
-            return true;
+public boolean sleepIn(boolean weekday,boolean vacation){
+        if(!weekday||vacation){
+        return true;
         }
         return false;
-    }
+        }
 //    Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
 //
 //    diff21(19) → 2
 //    diff21(10) → 11
 //    diff21(21) → 0
 
-    public int diff21(int n) {
-        if (n <= 21 ) {
-            return n = 21 - n;
-        } else {
-            return (n - 21) * 2;
+public int diff21(int n){
+        if(n<=21){
+        return n=21-n;
+        }else{
+        return(n-21)*2;
         }
-    }
+        }
 
 
 //    We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23. We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return true if we are in trouble.
@@ -34,11 +34,62 @@ public class Warmup-1 {
 //    parrotTrouble(true, 7) → false
 //    parrotTrouble(false, 6) → false
 
-    public boolean parrotTrouble(boolean talking, int hour) {
-        if (talking && ( 21 <= hour || hour < 7 )) {
-            return true;
+public boolean parrotTrouble(boolean talking,int hour){
+        if(talking&&(21<=hour||hour< 7)){
+        return true;
         }
         return false;
-    }
+        }
 
-}
+//        Given a string, return a new string where the first and last chars have been exchanged.
+//
+//        frontBack("code") → "eodc"
+//        frontBack("a") → "a"
+//        frontBack("ab") → "ba"
+
+public String frontBack(String str){
+        if(str==null||str.length()==0){
+        return str;
+        }
+        char[]cs=str.toCharArray();
+        char first=cs[0];
+        cs[0]=cs[cs.length-1];
+        cs[cs.length-1]=first;
+        return new String(cs);
+        }
+
+
+//
+//        Given a string, we'll say that the front is the first 3 chars of the string. If the string length is less than 3, the front is whatever is there. Return a new string which is 3 copies of the front.
+//
+//        front3("Java") → "JavJavJav"
+//        front3("Chocolate") → "ChoChoCho"
+//        front3("abc") → "abcabcabc"
+
+public String front3(String str){
+        String front;
+
+        if(str.length()>=3){
+        front=str.substring(0,3);  // ciag znakow
+        }
+        else{
+        front=str;
+        }
+        return front+front+front;
+        }
+
+
+//        Given a string, take the last char and return a new string with the last char added at the front and back, so "cat" yields "tcatt". The original string will be length 1 or more.
+//
+//        backAround("cat") → "tcatt"
+//        backAround("Hello") → "oHelloo"
+//        backAround("a") → "aaa"
+
+public String backAround(String str){
+        String mom;
+
+        mom=str.substring(str.length()-1);
+        return mom+str+mom;
+        }
+
+        }
