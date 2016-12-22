ecce95ed536ea784aa8fa9f950d37b3ed6221ac8
Wiktor <wiktorkalinowski@gmail.com>
2016-12-22 20:47:47 +0100

    code practice

--- CodingBat/SleepIn.java
+++ CodingBat/SleepIn.java
@@ -92,4 +92,43 @@ public String backAround(String str){
         return mom+str+mom;
         }
 
+
+//        Given a string, take the first 2 chars and return the string with the 2 chars added at both the front and back, so "kitten" yields"kikittenki". If the string length is less than 2, use whatever chars are there.
+//
+//        front22("kitten") → "kikittenki"
+//        front22("Ha") → "HaHaHa"
+//        front22("abc") → "ababcab"
+
+public String front22(String str){
+        if(str.length()<=2){
+        return str+str+str;
+
+        }
+
+
+//        Given a string, take the first 2 chars and return the string with the 2 chars added at both the front and back, so "kitten" yields"kikittenki". If the string length is less than 2, use whatever chars are there.
+//
+//        front22("kitten") → "kikittenki"
+//        front22("Ha") → "HaHaHa"
+//        front22("abc") → "ababcab"
+
+
+public String front22(String str){
+        String front;
+        String bolec;
+        if(str.length()<=2){
+        front=str+str+str;
+        return front;
+
+        }
+        if(str.length()>2){
+        bolec=str.substring(0,2);
+        return bolec+str+bolec;
+        }
+        else{
+        front=str;
+        }
+        return front;
+        }
+
         }
