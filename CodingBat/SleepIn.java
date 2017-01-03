8b2795c75640fa42f6abad005ed6c5a3ea008f39
Wiktor <wiktorkalinowski@gmail.com>
2017-01-03 21:49:03 +0100

    tasks

--- CodingBat/SleepIn.java
+++ CodingBat/SleepIn.java
@@ -328,3 +328,26 @@ delDel("adel") → "a"	"a"	OK
 delDel("aadelbb") → "aadelbb"	"aabb"	X	
 
 // TODO need to fix
+
+
+// We'll say that a number is "teen" if it is in the range 13..19 inclusive. Given 3 int values, return true if 1 or more of them are teen.
+
+// hasTeen(13, 20, 10) → true
+// hasTeen(20, 19, 10) → true
+// hasTeen(20, 10, 13) → true
+
+public boolean hasTeen(int a, int b, int c) {
+  return ((a >= 13 && a <= 19) || ( b >= 13 && b <= 19)  || (c >= 13 && c <= 19));
+}
+   
+// Return true if the given string begins with "mix", except the 'm' can be anything, so "pix", "9ix" .. all count.
+
+// mixStart("mix snacks") → true
+// mixStart("pix snacks") → true
+// mixStart("piz snacks") → false
+
+public boolean mixStart(String str) {
+   if (str.length() < 3) return false;
+   return (str.substring(1, 3).equals("ix"));
+}
+
