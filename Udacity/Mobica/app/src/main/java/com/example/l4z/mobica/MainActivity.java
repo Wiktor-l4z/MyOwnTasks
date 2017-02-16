2d599f6f9553e1741015a75a8c8bfff2fc151d4d
Wiktor <wiktorkalinowski@gmail.com>
2017-02-16 21:36:04 +0100

    Main loading page Mobica

---
+++ Udacity/Mobica/app/src/main/java/com/example/l4z/mobica/MainActivity.java
@@ -0,0 +1,140 @@
+package com.example.l4z.mobica;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+
+import static com.example.l4z.mobica.R.drawable.mobica_fix;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.mytoolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setBackgroundDrawable(backwardCompatibleDrawable(mobica_fix));
+        getSupportActionBar().setDisplayShowHomeEnabled(false);
+
+        Typeface myTypeFace = Typeface.createFromAsset(getAssets(), "RegencieLight.ttf");
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setTypeface(myTypeFace);
+
+
+
+
+        ImageView uxImageView = (ImageView) findViewById(R.id.ux_ui);
+        uxImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("http://www.mobica.com/services-technologies/user-experience-user-interface-uxui"));
+                startActivity(intent);
+            }
+        });
+
+        ImageView qualityImageView = (ImageView) findViewById(R.id.quality_assurance);
+        qualityImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("http://www.mobica.com/services-technologies/quality-assurance"));
+                startActivity(intent);
+            }
+        });
+
+        ImageView techImageView = (ImageView) findViewById(R.id.technology_colsulting);
+        techImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("http://www.mobica.com/services-technologies/technology-consulting"));
+                startActivity(intent);
+            }
+        });
+
+        ImageView softImageView = (ImageView) findViewById(R.id.software_engineering);
+        softImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("http://www.mobica.com/services-technologies/software-engineering"));
+                startActivity(intent);
+            }
+        });
+
+        ImageView linkedInImageView = (ImageView) findViewById(R.id.linkedIn);
+        linkedInImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://www.linkedin.com/company/mobica"));
+                startActivity(intent);
+            }
+        });
+
+        ImageView facebookImageView = (ImageView) findViewById(R.id.facebook);
+        facebookImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://www.facebook.com/MobicaLimited"));
+                startActivity(intent);
+            }
+        });
+        ImageView ytImageView = (ImageView) findViewById(R.id.youtube);
+        ytImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://www.youtube.com/channel/UC3c5EpJZ2pqI2GPapiRt8uA"));
+                startActivity(intent);
+            }
+        });
+
+        ImageView twitterImageView = (ImageView) findViewById(R.id.twitter);
+        twitterImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://twitter.com/_Mobica"));
+                startActivity(intent);
+            }
+        });
+    }
+
+    private Drawable backwardCompatibleDrawable(int res) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            return getDrawable(res);
+        }
+        return getResources().getDrawable(res);
+    }
+}
