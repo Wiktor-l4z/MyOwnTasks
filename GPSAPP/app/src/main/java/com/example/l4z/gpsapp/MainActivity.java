3522cee8dc2cc3489e49b65b24a2db6de8a9d482
Wiktor <wiktorkalinowski@gmail.com>
2017-04-23 23:40:28 +0200

    final GPS restaurant app

--- GPSAPP/app/src/main/java/com/example/l4z/gpsapp/MainActivity.java
+++ GPSAPP/app/src/main/java/com/example/l4z/gpsapp/MainActivity.java
@@ -1,90 +1,87 @@
 package com.example.l4z.gpsapp;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
 
-    Button btnHybrid, btnSatellite, btnMap;
-    GoogleMap m_map;
+    private Kwota kwota;
+    private Kuchnia kuchnia;
+    private List<Restauracja> restauracjas;
+    private GoogleMap m_map;
     boolean mapReady = false;
+
+
+    static final CameraPosition LUBLIN = CameraPosition.builder()
+            .target(new LatLng(51.2348891, 22.5468983))
+            .zoom(13)
+            .bearing(0)
+            .tilt(30)
+            .build();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        kwota = (Kwota) getIntent().getSerializableExtra(KwotaActivity.KWOTA_KEY);
+        kuchnia = (Kuchnia) getIntent().getSerializableExtra(KuchniaActivity.KUCHNIA_KEY);
 
-        btnHybrid = (Button)findViewById(R.id.btnHybrid);
-        btnHybrid.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mapReady)
-                    m_map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-            }
-        });
-        btnSatellite = (Button)findViewById(R.id.btnSatellite);
-        btnSatellite.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mapReady)
-                    m_map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-            }
-        });
-        btnMap = (Button)findViewById(R.id.btnMap);
-        btnMap.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mapReady)
-                    m_map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-            }
-        });
+        restauracjas = BazaKuchni.getInstance().getRestauracja(kwota, kuchnia);
 
-        MapFragment mapFragment = (MapFragment)getFragmentManager().findFragmentById(R.id.map);
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
     }
 
-
-    @Override
-    public void onMapReady(GoogleMap map) {
-        mapReady = true;
-        m_map = map;
-        LatLng newYork = new LatLng(40.7484, -73.9857);
-        CameraPosition target = CameraPosition.builder().target(newYork).zoom(14).build();
+    private void flyTo(CameraPosition target) {
         m_map.moveCamera(CameraUpdateFactory.newCameraPosition(target));
     }
 
     @Override
+    public void onMapReady(GoogleMap map) {
+
+        mapReady = true;
+        m_map = map;
+        m_map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(41.889, -87.622), 16));
+        for (Restauracja r : restauracjas) {
+            m_map.addMarker(r.getMarkerOption());
+        }
+        flyTo(LUBLIN);
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu,menu);
+        // Inflate the main_menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public Resources getResources() {
+        return super.getResources();
     }
 }
